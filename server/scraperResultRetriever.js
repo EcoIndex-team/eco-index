c90eb5e0ced568c37c4449690eab38de7870dedb
ZeSved <timothy.korell@gmail.com>
2024-11-18 18:10:32 +0100

    added config again

--- server/scraperResultRetriever.js
+++ server/scraperResultRetriever.js
@@ -15,7 +15,8 @@ export default async function scraperResultRetriever(storeName, barcode) {
     const exec = util.promisify(child_process.exec)
 
     const { stdout } = await exec(
-        `python server/scraper/scraper.py ${storeName} ${barcode}`
+        `python server/scraper/scraper.py ${storeName} ${barcode}`,
+        { shell: true }
         // (error, out) => {
         //     if (error) {
         //         console.error('exec error: ', error)
